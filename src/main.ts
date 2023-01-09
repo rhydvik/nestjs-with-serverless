95b550b5121fcc8952756123cc1bb322cd654c05
VIKRAM THAKUR <rhydvik@gmail.com>
2023-01-09 20:50:59 +0530

    serverless with nestjs project

---
+++ src/main.ts
@@ -0,0 +1,15 @@
+import { NestFactory } from '@nestjs/core';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import { INestApplication } from '@nestjs/common';
+import { Express } from 'express';
+import { AppModule } from './app.module';
+
+export async function createApp(
+  expressApp: Express,
+): Promise<INestApplication> {
+  const app = await NestFactory.create(
+    AppModule,
+    new ExpressAdapter(expressApp),
+  );
+  return app;
+}
